53cf87624422eec16895c14bdfacd6a8d57a28f3
dhickok <dhickok@iastate.edu>
2016-07-15 19:56:31 -0500

    Second commit, working matrix storage

--- src/rumors/RumorReader.java
+++ src/rumors/RumorReader.java
@@ -9,25 +9,34 @@ public class RumorReader {
 			
 			File f = new File(fName);
 			Scanner fIn = new Scanner(f);
+			int[][] brokerArray = null;
 			int numBrokers = 0;
 			int count = 0;
 			
 			while (fIn.hasNextLine()){
-				count++;
-				if (count == 1){
-					numBrokers = fIn.nextInt();
-					int[][] brokerArray = new int[numBrokers][numBrokers];
-					System.out.println("The number of Brokers is: " + numBrokers);
-				}
-				else{
-					int numContacts = fIn.nextInt();
-					for (int i = 0; i < numContacts; i++){
-						for (int j = 0; j < 2; j++){
-							
+				numBrokers = fIn.nextInt();
+				brokerArray = new int[numBrokers][numBrokers];
+				System.out.println("The number of Brokers is: " + numBrokers);
+				int row = 0;
+					for (int x = 0; x < numBrokers; x++){
+						int numContacts = fIn.nextInt();
+						for (int i = 0; i < numContacts; i++){
+								int broker = fIn.nextInt();
+								int timeTo = fIn.nextInt();
+								//System.out.println("The broker # is: " + broker);
+								System.out.println("The time to broker " + broker + " is: " + timeTo);
+								brokerArray[x][broker-1] = timeTo;
+						}
+						//numContacts = 0;
+						
+					}
+					for (int i = 0; i < brokerArray.length; i++){
+						System.out.println("");
+						for(int j = 0; j < brokerArray.length; j++){
+							System.out.print(brokerArray[i][j]);
 						}
 					}
-					
-				}
+					System.out.println("");
 			}
 		}
 }
